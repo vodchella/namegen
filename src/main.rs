5bf3041c18c12b22caf5a283f54437e6b278fd83
vodchella <twister.kz@gmail.com>
2025-05-08 22:49:18 +0500

    Functions returns vecs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use rand::Rng;
 
 const VOW: [&str; 5] = ["a", "e", "i", "o", "u"];
-const CON: [&str; 22] = [
+const CON: [&str; 28] = [
     "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n",
-    "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "th"
+    "p", "q", "r", "s", "t", "v", "w", "x", "y", "z",
+    "th", "ch", "sh", "ph", "wh", "gh", "qu"
 ];
 
 
@@ -19,28 +20,30 @@ fn get_rnd_con() -> &'static str {
     CON[get_rnd_num(CON.len())]
 }
 
-fn get_rnd_syl() -> String {
+fn get_rnd_syl() -> Vec<&'static str> {
     let template = get_rnd_num(3);
+    let mut symbols: Vec<&str> = Vec::new();
     match template {
-        0 => format!("{}{}", get_rnd_vow(), get_rnd_con()),
-        1 => format!("{}{}", get_rnd_con(), get_rnd_vow()),
-        2 => format!("{}{}{}", get_rnd_con(), get_rnd_vow(), get_rnd_con()),
+        0 => symbols.extend([get_rnd_vow(), get_rnd_con()]),
+        1 => symbols.extend([get_rnd_con(), get_rnd_vow()]),
+        2 => symbols.extend([get_rnd_con(), get_rnd_vow(), get_rnd_con()]),
         _ => todo!(),
     }
+    symbols
 }
 
-fn get_rnd_word() -> String {
-    let mut result: String = "".to_string();
+fn get_rnd_word() -> Vec<&'static str> {
+    let mut word: Vec<&str> = Vec::new();
     let syl_cnt = get_rnd_num(3) + 1;
     for _i in 0 .. syl_cnt {
-        result += &get_rnd_syl();
+        word.append(&mut get_rnd_syl());
     }
-    result
+    word
 }
 
 
 fn main() {
     for _i in 1 .. 10 {
-        println!("{}", get_rnd_word());
+        println!("{}", get_rnd_word().join(""));
     }
 }
